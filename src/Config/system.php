05661b6b1fdded0fbfed5be317b2f226281dd8a6
Arun chahar <info@vfixtechnology.com>
2025-04-12 18:48:06 +0530

    Initial project commit version 1.0.1

---
+++ src/Config/system.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+    [
+        'key'    => 'sales.payment_methods.cashfree',
+        'name'   => 'Cashfree',
+        'info' => 'Cashfree extension created for Bagisto by <a href="https://www.vfixtechnology.com" target="_blank" style="color: blue;">Vfix Technology</a>.',
+        'sort'   => 4,
+        'fields' => [
+            [
+                'name'          => 'Cashfree',
+                'title'         => 'Title',
+                'type'          => 'text',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+            [
+                'name'          => 'Pay via card, upi, bank transfer',
+                'title'         => 'Description',
+                'type'          => 'textarea',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+            [
+                'name'          => 'image',
+                'title'         => 'Logo',
+                'type'          => 'image',
+                'channel_based' => false,
+                'locale_based'  => false,
+                'validation'    => 'mimes:bmp,jpeg,jpg,png,webp',
+            ],
+            [
+                'name'          => 'key_id',
+                'title'         => 'key id',
+                'type'          => 'text',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+			[
+                'name'          => 'secret',
+                'title'         => 'key secret',
+                'type'          => 'text',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ],
+			[
+                'name'    => 'website',
+                'title'   => 'Website Status',
+                'type'    => 'select',
+                'validation'    => 'required',
+                'options' => [
+                    [
+                        'title' => 'Staging',
+                        'value' => 'sandbox',
+                    ], [
+                        'title' => 'Live',
+                        'value' => 'production',
+                    ],
+                ],
+            ],
+            [
+                'name'          => 'active',
+                'title'         => 'Status',
+                'type'          => 'boolean',
+                'validation'    => 'required',
+                'channel_based' => false,
+                'locale_based'  => true,
+            ]
+        ]
+    ]
+];
